32993751b76c5f883e9315a77c2042017e39f5a4
Charlotte Margus <charlotte.boyce@hotmail.fr>
2020-02-12 18:08:28 +0100

    last version

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Stylish house',
+  address: '5 Quensmill road',
+  description: 'A lovely appartment to stay',
+  price_per_night: 65,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Guest suite',
+  address: 'Euston Rd',
+  description: 'Perfect for a week-end in London',
+  price_per_night: 110,
+  number_of_guests: 2
+)
